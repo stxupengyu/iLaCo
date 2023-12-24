1bdc02ada827bfaad20452edb05d480b376cd8ee
Pangyu <19121638@bjtu.edu.cn>
2023-12-24 21:01:09 +0800

    Update run_ilaco.sh

--- script/run_ilaco.sh
+++ script/run_ilaco.sh
@@ -5,19 +5,19 @@ RATE=$2
 GPUID=$3
 
 if [ "$DATASET" = "eurlex" ]; then
-    python main.py --gpuid $GPUID --data_dir $DATASET \
+    python main.py --gpuid $GPUID --dataset $DATASET \
     --epoch 50 --batch 128  \
     --alpha 0.7 --beta 0.001 \
     --rho $RATE
 
 elif [ "$DATASET" = "rcv" ]; then
-    python main.py --gpuid $GPUID --data_dir $DATASET \
+    python main.py --gpuid $GPUID --dataset $DATASET \
     --epoch 50 --batch 256  \
     --alpha 0.7 --beta 0.05 \
     --rho $RATE
 
 elif [ "$DATASET" = "aapd" ]; then
-    python main.py --gpuid $GPUID --data_dir $DATASET \
+    python main.py --gpuid $GPUID --dataset $DATASET \
     --epoch 50 --batch 256  \
     --alpha 0.7 --beta 0.05 \
     --rho $RATE
